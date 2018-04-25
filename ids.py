51f5b79cac4c64e7983e250942ffea33d5f985a6
Kyrylo Chernyshov <kc875@cornell.edu>
2018-04-25 16:12:30 -0400

    Add name-to-id map function stub

---
+++ ids.py
@@ -0,0 +1,13 @@
+# This file contains the functions that map cards to card IDs.
+
+
+def card_to_id(name: str, rating: int) -> int:
+    """
+    This function returns the ID associated with the card, using its name and rating.
+    
+    @rtype: int
+    @param name: The name of the player on the card.
+    @param rating: The rating of the card.
+    @return: The unique numerical ID of the card.
+    """
+    pass
