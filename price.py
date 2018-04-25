6349bbfbee03dbc41050109a184c9abb43fa6bf1
Kyrylo Chernyshov <kc875@cornell.edu>
2018-04-25 12:17:54 -0400

    Add price function stubs

---
+++ price.py
@@ -0,0 +1,23 @@
+# This file will contain the functions that fetch the average current price of a card, as well as price ranges
+
+
+def current_price(card_id: int) -> int:
+    """
+    Returns the current average price for the card. This is the average price that the card is currently trading for.
+
+    @rtype: int
+    @param card_id: The id of the card.
+    @return: The current average price for the card.
+    """
+    pass
+
+
+def price_range(card_id: int) -> tuple:
+    """
+    Returns the price range for the card.
+
+    @rtype: tuple
+    @param card_id: The id of the card.
+    @return: The price range for the card, in the form (lower, upper).
+    """
+    pass
